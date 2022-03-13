b846de63e47e2ea3e452432c2e316628f6960b7f
Adam Gregory <agregory@actifi.com>
2022-03-13 17:49:58 -0500

    removing some boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
 import Phaser1 from "@/components/Phaser1.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    
+    <Phaser1></Phaser1>
   </main>
 </template>
